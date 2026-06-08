4fd7b79d118f5353a91117253c0aa020b002feab
Thomas97460 <t.collet974@outlook.fr>
2026-06-08 19:26:33 +0200

    bump: version 0.1.38

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "quota-tracker dev environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }:
